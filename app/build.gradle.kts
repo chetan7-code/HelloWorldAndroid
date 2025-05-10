c1f725409d7cc79da730a1075e10e578fa1d6310
Chetan <50010056@mahindra.com>
2025-05-10 17:12:29 +0530

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.yourappname"
+    namespace = "com.example.helloworld"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.yourappname"
+        applicationId = "com.example.helloworld"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
